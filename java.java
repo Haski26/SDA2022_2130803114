5f8cc9a0ecf754baf9e3fdbdbec25b3317fff439
unknown <didiputrahaski19@gmail.com>
2022-09-22 13:44:47 +0700

    coba kak

---
+++ java.java
@@ -0,0 +1,15 @@
+/**
+ * java
+ */
+import java.util.Scanner;
+public class java {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       
+System.out.print("Enter your name: ");
+String name = in.nextLine();
+System.out.println("Name is: " + name);
+in.close();
+    }
+}
